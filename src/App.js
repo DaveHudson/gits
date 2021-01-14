bdae40a12d5d13d0cf837df771ea7afaddd9837d
Dave Hudson <dave@applification.net>
2021-01-14 22:41:39 +0000

    Yet more stuff

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>Do some other stuff too...</p>
+        <p>Yet more stuff</p>
         <a
           className="App-link"
           href="https://reactjs.org"
